cab764e938a77d2dad805c1e65f319ea09d6d70b
argalad <github@argalad.com>
2026-08-27 23:17:37 +0200

    Open-BFME5: convert five errands handed on through a table

---
+++ Code/GameEngine/Source/Common/BfmeOneHundredSeventyFive.cpp
@@ -0,0 +1,550 @@
+// Five more errands handed on through a table of methods.
+
+class BfmeTargetBL
+{
+public:
+	virtual void bfmeSpare000BL(void) = 0;
+	virtual void bfmeSpare001BL(void) = 0;
+	virtual void bfmeSpare002BL(void) = 0;
+	virtual void bfmeSpare003BL(void) = 0;
+	virtual void bfmeSpare004BL(void) = 0;
+	virtual void bfmeSpare005BL(void) = 0;
+	virtual void bfmeSpare006BL(void) = 0;
+	virtual void bfmeSpare007BL(void) = 0;
+	virtual void bfmeSpare008BL(void) = 0;
+	virtual void bfmeSpare009BL(void) = 0;
+	virtual void bfmeSpare010BL(void) = 0;
+	virtual void bfmeSpare011BL(void) = 0;
+	virtual void bfmeSpare012BL(void) = 0;
+	virtual void bfmeSpare013BL(void) = 0;
+	virtual void bfmeSpare014BL(void) = 0;
+	virtual void bfmeSpare015BL(void) = 0;
+	virtual void bfmeSpare016BL(void) = 0;
+	virtual void bfmeSpare017BL(void) = 0;
+	virtual void bfmeSpare018BL(void) = 0;
+	virtual void bfmeSpare019BL(void) = 0;
+	virtual void bfmeSpare020BL(void) = 0;
+	virtual void bfmeSpare021BL(void) = 0;
+	virtual void bfmeSpare022BL(void) = 0;
+	virtual void bfmeSpare023BL(void) = 0;
+	virtual void bfmeSpare024BL(void) = 0;
+	virtual void bfmeSpare025BL(void) = 0;
+	virtual void bfmeSpare026BL(void) = 0;
+	virtual void bfmeSpare027BL(void) = 0;
+	virtual void bfmeSpare028BL(void) = 0;
+	virtual void bfmeSpare029BL(void) = 0;
+	virtual void bfmeSpare030BL(void) = 0;
+	virtual void bfmeSpare031BL(void) = 0;
+	virtual void bfmeSpare032BL(void) = 0;
+	virtual void bfmeSpare033BL(void) = 0;
+	virtual void bfmeSpare034BL(void) = 0;
+	virtual void bfmeSpare035BL(void) = 0;
+	virtual void bfmeSpare036BL(void) = 0;
+	virtual void bfmeSpare037BL(void) = 0;
+	virtual void bfmeSpare038BL(void) = 0;
+	virtual void bfmeSpare039BL(void) = 0;
+	virtual void bfmeSpare040BL(void) = 0;
+	virtual void bfmeSpare041BL(void) = 0;
+	virtual void bfmeSpare042BL(void) = 0;
+	virtual void bfmeSpare043BL(void) = 0;
+	virtual void bfmeSpare044BL(void) = 0;
+	virtual void bfmeSpare045BL(void) = 0;
+	virtual void bfmeSpare046BL(void) = 0;
+	virtual void bfmeSpare047BL(void) = 0;
+	virtual void bfmeSpare048BL(void) = 0;
+	virtual void bfmeSpare049BL(void) = 0;
+	virtual void bfmeSpare050BL(void) = 0;
+	virtual void bfmeSpare051BL(void) = 0;
+	virtual void bfmeSpare052BL(void) = 0;
+	virtual void bfmeSpare053BL(void) = 0;
+	virtual void bfmeSpare054BL(void) = 0;
+	virtual void bfmeSpare055BL(void) = 0;
+	virtual void bfmeSpare056BL(void) = 0;
+	virtual void bfmeSpare057BL(void) = 0;
+	virtual void bfmeSpare058BL(void) = 0;
+	virtual void bfmeSpare059BL(void) = 0;
+	virtual void bfmeSpare060BL(void) = 0;
+	virtual void bfmeSpare061BL(void) = 0;
+	virtual void bfmeSpare062BL(void) = 0;
+	virtual void bfmeSpare063BL(void) = 0;
+	virtual void bfmeSpare064BL(void) = 0;
+	virtual void bfmeSpare065BL(void) = 0;
+	virtual void bfmeSpare066BL(void) = 0;
+	virtual void bfmeSpare067BL(void) = 0;
+	virtual void bfmeSpare068BL(void) = 0;
+	virtual void bfmeSpare069BL(void) = 0;
+	virtual void bfmeSpare070BL(void) = 0;
+	virtual void bfmeSpare071BL(void) = 0;
+	virtual void bfmeSpare072BL(void) = 0;
+	virtual void bfmeSpare073BL(void) = 0;
+	virtual void bfmeSpare074BL(void) = 0;
+	virtual void bfmeSpare075BL(void) = 0;
+	virtual void bfmeSpare076BL(void) = 0;
+	virtual void bfmeSpare077BL(void) = 0;
+	virtual void bfmeSpare078BL(void) = 0;
+	virtual void bfmeSpare079BL(void) = 0;
+	virtual void bfmeSpare080BL(void) = 0;
+	virtual void bfmeSpare081BL(void) = 0;
+	virtual void bfmeSpare082BL(void) = 0;
+	virtual void bfmeSpare083BL(void) = 0;
+	virtual void bfmeSpare084BL(void) = 0;
+	virtual void bfmeSpare085BL(void) = 0;
+	virtual void bfmeSpare086BL(void) = 0;
+	virtual void bfmeSpare087BL(void) = 0;
+	virtual void bfmeSpare088BL(void) = 0;
+	virtual void bfmeSpare089BL(void) = 0;
+	virtual void bfmeSpare090BL(void) = 0;
+	virtual void bfmeSpare091BL(void) = 0;
+	virtual void bfmeSpare092BL(void) = 0;
+	virtual void bfmeSpare093BL(void) = 0;
+	virtual void bfmeSpare094BL(void) = 0;
+	virtual void bfmeSpare095BL(void) = 0;
+	virtual void bfmeSpare096BL(void) = 0;
+	virtual void bfmeSpare097BL(void) = 0;
+	virtual void bfmeSpare098BL(void) = 0;
+	virtual void bfmeSpare099BL(void) = 0;
+	virtual void bfmeSpare100BL(void) = 0;
+	virtual void bfmeSpare101BL(void) = 0;
+	virtual void bfmeSpare102BL(void) = 0;
+	virtual void bfmeSpare103BL(void) = 0;
+	virtual void bfmeSpare104BL(void) = 0;
+	virtual void bfmeSpare105BL(void) = 0;
+	virtual void bfmeDoBL(int what) = 0;
+};
+
+class BfmeThingBL
+{
+public:
+	void bfmeSendBL(unsigned char what);
+
+private:
+	unsigned char m_bfmeHead[0x34];		// 0x00
+	BfmeTargetBL *m_bfmeTarget;		// 0x34
+};
+
+void BfmeThingBL::bfmeSendBL(unsigned char what)
+{
+	m_bfmeTarget->bfmeDoBL(what);
+}
+
+class BfmeTargetBM
+{
+public:
+	virtual void bfmeSpare000BM(void) = 0;
+	virtual void bfmeSpare001BM(void) = 0;
+	virtual void bfmeSpare002BM(void) = 0;
+	virtual void bfmeSpare003BM(void) = 0;
+	virtual void bfmeSpare004BM(void) = 0;
+	virtual void bfmeSpare005BM(void) = 0;
+	virtual void bfmeSpare006BM(void) = 0;
+	virtual void bfmeSpare007BM(void) = 0;
+	virtual void bfmeSpare008BM(void) = 0;
+	virtual void bfmeSpare009BM(void) = 0;
+	virtual void bfmeSpare010BM(void) = 0;
+	virtual void bfmeSpare011BM(void) = 0;
+	virtual void bfmeSpare012BM(void) = 0;
+	virtual void bfmeSpare013BM(void) = 0;
+	virtual void bfmeSpare014BM(void) = 0;
+	virtual void bfmeSpare015BM(void) = 0;
+	virtual void bfmeSpare016BM(void) = 0;
+	virtual void bfmeSpare017BM(void) = 0;
+	virtual void bfmeSpare018BM(void) = 0;
+	virtual void bfmeSpare019BM(void) = 0;
+	virtual void bfmeSpare020BM(void) = 0;
+	virtual void bfmeSpare021BM(void) = 0;
+	virtual void bfmeSpare022BM(void) = 0;
+	virtual void bfmeSpare023BM(void) = 0;
+	virtual void bfmeSpare024BM(void) = 0;
+	virtual void bfmeSpare025BM(void) = 0;
+	virtual void bfmeSpare026BM(void) = 0;
+	virtual void bfmeSpare027BM(void) = 0;
+	virtual void bfmeSpare028BM(void) = 0;
+	virtual void bfmeSpare029BM(void) = 0;
+	virtual void bfmeSpare030BM(void) = 0;
+	virtual void bfmeSpare031BM(void) = 0;
+	virtual void bfmeSpare032BM(void) = 0;
+	virtual void bfmeSpare033BM(void) = 0;
+	virtual void bfmeSpare034BM(void) = 0;
+	virtual void bfmeSpare035BM(void) = 0;
+	virtual void bfmeSpare036BM(void) = 0;
+	virtual void bfmeSpare037BM(void) = 0;
+	virtual void bfmeSpare038BM(void) = 0;
+	virtual void bfmeSpare039BM(void) = 0;
+	virtual void bfmeSpare040BM(void) = 0;
+	virtual void bfmeSpare041BM(void) = 0;
+	virtual void bfmeSpare042BM(void) = 0;
+	virtual void bfmeSpare043BM(void) = 0;
+	virtual void bfmeSpare044BM(void) = 0;
+	virtual void bfmeSpare045BM(void) = 0;
+	virtual void bfmeSpare046BM(void) = 0;
+	virtual void bfmeSpare047BM(void) = 0;
+	virtual void bfmeSpare048BM(void) = 0;
+	virtual void bfmeSpare049BM(void) = 0;
+	virtual void bfmeSpare050BM(void) = 0;
+	virtual void bfmeSpare051BM(void) = 0;
+	virtual void bfmeSpare052BM(void) = 0;
+	virtual void bfmeSpare053BM(void) = 0;
+	virtual void bfmeSpare054BM(void) = 0;
+	virtual void bfmeSpare055BM(void) = 0;
+	virtual void bfmeSpare056BM(void) = 0;
+	virtual void bfmeSpare057BM(void) = 0;
+	virtual void bfmeSpare058BM(void) = 0;
+	virtual void bfmeSpare059BM(void) = 0;
+	virtual void bfmeSpare060BM(void) = 0;
+	virtual void bfmeSpare061BM(void) = 0;
+	virtual void bfmeSpare062BM(void) = 0;
+	virtual void bfmeSpare063BM(void) = 0;
+	virtual void bfmeSpare064BM(void) = 0;
+	virtual void bfmeSpare065BM(void) = 0;
+	virtual void bfmeSpare066BM(void) = 0;
+	virtual void bfmeSpare067BM(void) = 0;
+	virtual void bfmeSpare068BM(void) = 0;
+	virtual void bfmeSpare069BM(void) = 0;
+	virtual void bfmeSpare070BM(void) = 0;
+	virtual void bfmeSpare071BM(void) = 0;
+	virtual void bfmeSpare072BM(void) = 0;
+	virtual void bfmeSpare073BM(void) = 0;
+	virtual void bfmeSpare074BM(void) = 0;
+	virtual void bfmeSpare075BM(void) = 0;
+	virtual void bfmeSpare076BM(void) = 0;
+	virtual void bfmeSpare077BM(void) = 0;
+	virtual void bfmeSpare078BM(void) = 0;
+	virtual void bfmeSpare079BM(void) = 0;
+	virtual void bfmeSpare080BM(void) = 0;
+	virtual void bfmeSpare081BM(void) = 0;
+	virtual void bfmeSpare082BM(void) = 0;
+	virtual void bfmeSpare083BM(void) = 0;
+	virtual void bfmeSpare084BM(void) = 0;
+	virtual void bfmeSpare085BM(void) = 0;
+	virtual void bfmeSpare086BM(void) = 0;
+	virtual void bfmeSpare087BM(void) = 0;
+	virtual void bfmeSpare088BM(void) = 0;
+	virtual void bfmeSpare089BM(void) = 0;
+	virtual void bfmeSpare090BM(void) = 0;
+	virtual void bfmeSpare091BM(void) = 0;
+	virtual void bfmeSpare092BM(void) = 0;
+	virtual void bfmeSpare093BM(void) = 0;
+	virtual void bfmeSpare094BM(void) = 0;
+	virtual void bfmeSpare095BM(void) = 0;
+	virtual void bfmeSpare096BM(void) = 0;
+	virtual void bfmeSpare097BM(void) = 0;
+	virtual void bfmeSpare098BM(void) = 0;
+	virtual void bfmeSpare099BM(void) = 0;
+	virtual unsigned char bfmeDoBM(void) = 0;
+};
+
+struct BfmeOwnerBM
+{
+	unsigned char m_bfmeHead[0x204];	// 0x000
+	BfmeTargetBM *m_bfmeTarget;		// 0x204
+};
+
+class BfmeSubBM
+{
+public:
+	unsigned char bfmeAskBM(void) const;
+
+	unsigned char m_bfmeHead[4];		// 0x0
+};
+
+struct BfmeHostBM
+{
+	BfmeOwnerBM *m_bfmeOwner;		// 0x000
+	unsigned char m_bfmeGap[0xd8];		// 0x004
+	BfmeSubBM m_bfmeSub;			// 0x0dc
+};
+
+unsigned char BfmeSubBM::bfmeAskBM(void) const
+{
+	const BfmeHostBM *host = (const BfmeHostBM *)((const char *)this - 0xdc);
+	BfmeTargetBM *target = host->m_bfmeOwner->m_bfmeTarget;
+
+	if (target == 0)
+		return 0;
+
+	return target->bfmeDoBM();
+}
+
+class BfmeSecondBO
+{
+public:
+	virtual void bfmeSpare000BO(void);
+	virtual void bfmeAlsoBO(void);
+};
+
+class BfmeFirstBO
+{
+public:
+	virtual void bfmeSpare000BP(void);
+	virtual void bfmeSpare001BP(void);
+	virtual void bfmeSpare002BP(void);
+	virtual void bfmeDoBO(void);
+};
+
+class BfmeThingBO
+{
+public:
+	void bfmeGoBO(void);
+
+private:
+	unsigned char m_bfmeHead[0x25c];	// 0x000
+	BfmeSecondBO m_bfmeSecond;		// 0x25c
+	unsigned char m_bfmeGap[0x14c];		// 0x260
+	BfmeFirstBO m_bfmeFirst;		// 0x3ac
+};
+
+void BfmeThingBO::bfmeGoBO(void)
+{
+	m_bfmeFirst.bfmeDoBO();
+	m_bfmeSecond.bfmeAlsoBO();
+}
+
+class BfmeThingBP;
+
+class BfmeMakerBP
+{
+public:
+	virtual void bfmeSpare000BQ(void) = 0;
+	virtual void bfmeSpare001BQ(void) = 0;
+	virtual void bfmeSpare002BQ(void) = 0;
+	virtual void bfmeSpare003BQ(void) = 0;
+	virtual void bfmeSpare004BQ(void) = 0;
+	virtual void bfmeSpare005BQ(void) = 0;
+	virtual void bfmeSpare006BQ(void) = 0;
+	virtual void bfmeSpare007BQ(void) = 0;
+	virtual void bfmeSpare008BQ(void) = 0;
+	virtual void bfmeSpare009BQ(void) = 0;
+	virtual void bfmeSpare010BQ(void) = 0;
+	virtual void bfmeSpare011BQ(void) = 0;
+	virtual void bfmeSpare012BQ(void) = 0;
+	virtual void bfmeSpare013BQ(void) = 0;
+	virtual void bfmeSpare014BQ(void) = 0;
+	virtual void bfmeSpare015BQ(void) = 0;
+	virtual void bfmeSpare016BQ(void) = 0;
+	virtual void bfmeSpare017BQ(void) = 0;
+	virtual void bfmeSpare018BQ(void) = 0;
+	virtual void bfmeSpare019BQ(void) = 0;
+	virtual void bfmeSpare020BQ(void) = 0;
+	virtual void bfmeSpare021BQ(void) = 0;
+	virtual void bfmeSpare022BQ(void) = 0;
+	virtual void bfmeSpare023BQ(void) = 0;
+	virtual void bfmeSpare024BQ(void) = 0;
+	virtual void bfmeSpare025BQ(void) = 0;
+	virtual BfmeThingBP *bfmeMakeBP(void) = 0;
+};
+
+class BfmeThingBP
+{
+public:
+	virtual void bfmeSpare000BR(void) = 0;
+	virtual void bfmeSpare001BR(void) = 0;
+	virtual void bfmeSpare002BR(void) = 0;
+	virtual void bfmeSpare003BR(void) = 0;
+	virtual void bfmeSpare004BR(void) = 0;
+	virtual void bfmeSpare005BR(void) = 0;
+	virtual unsigned char bfmeDoBP(int spare) = 0;
+};
+
+struct BfmeOwnerBP
+{
+	unsigned char m_bfmeHead[0x1fc];	// 0x000
+	BfmeMakerBP *m_bfmeMaker;		// 0x1fc
+};
+
+class BfmeSubBP
+{
+public:
+	unsigned char bfmeAskBP(int spare) const;
+
+	unsigned char m_bfmeHead[4];		// 0x0
+};
+
+struct BfmeHostBP
+{
+	BfmeOwnerBP *m_bfmeOwner;		// 0x000
+	unsigned char m_bfmeGap[0x334];		// 0x004
+	BfmeSubBP m_bfmeSub;			// 0x338
+};
+
+unsigned char BfmeSubBP::bfmeAskBP(int spare) const
+{
+	const BfmeHostBP *host = (const BfmeHostBP *)((const char *)this - 0x338);
+	BfmeThingBP *thing = host->m_bfmeOwner->m_bfmeMaker->bfmeMakeBP();
+
+	if (thing == 0)
+		return 0;
+
+	return thing->bfmeDoBP(spare);
+}
+
+class BfmeMadeBQ
+{
+public:
+	virtual void bfmeSpare000BS(void) = 0;
+	virtual void bfmeSpare001BS(void) = 0;
+	virtual void bfmeSpare002BS(void) = 0;
+	virtual void bfmeSpare003BS(void) = 0;
+	virtual void bfmeSpare004BS(void) = 0;
+	virtual void bfmeSpare005BS(void) = 0;
+	virtual void bfmeSpare006BS(void) = 0;
+	virtual void bfmeDoBQ(void) = 0;
+};
+
+class BfmeThingBQ
+{
+public:
+	virtual void bfmeSpare000BT(void) = 0;
+	virtual void bfmeSpare001BT(void) = 0;
+	virtual void bfmeSpare002BT(void) = 0;
+	virtual void bfmeSpare003BT(void) = 0;
+	virtual void bfmeSpare004BT(void) = 0;
+	virtual void bfmeSpare005BT(void) = 0;
+	virtual void bfmeSpare006BT(void) = 0;
+	virtual void bfmeSpare007BT(void) = 0;
+	virtual void bfmeSpare008BT(void) = 0;
+	virtual void bfmeSpare009BT(void) = 0;
+	virtual void bfmeSpare010BT(void) = 0;
+	virtual void bfmeSpare011BT(void) = 0;
+	virtual void bfmeSpare012BT(void) = 0;
+	virtual void bfmeSpare013BT(void) = 0;
+	virtual void bfmeSpare014BT(void) = 0;
+	virtual void bfmeSpare015BT(void) = 0;
+	virtual void bfmeSpare016BT(void) = 0;
+	virtual void bfmeSpare017BT(void) = 0;
+	virtual void bfmeSpare018BT(void) = 0;
+	virtual void bfmeSpare019BT(void) = 0;
+	virtual void bfmeSpare020BT(void) = 0;
+	virtual void bfmeSpare021BT(void) = 0;
+	virtual void bfmeSpare022BT(void) = 0;
+	virtual void bfmeSpare023BT(void) = 0;
+	virtual void bfmeSpare024BT(void) = 0;
+	virtual void bfmeSpare025BT(void) = 0;
+	virtual void bfmeSpare026BT(void) = 0;
+	virtual void bfmeSpare027BT(void) = 0;
+	virtual void bfmeSpare028BT(void) = 0;
+	virtual void bfmeSpare029BT(void) = 0;
+	virtual void bfmeSpare030BT(void) = 0;
+	virtual void bfmeSpare031BT(void) = 0;
+	virtual void bfmeSpare032BT(void) = 0;
+	virtual void bfmeSpare033BT(void) = 0;
+	virtual void bfmeSpare034BT(void) = 0;
+	virtual void bfmeSpare035BT(void) = 0;
+	virtual void bfmeSpare036BT(void) = 0;
+	virtual void bfmeSpare037BT(void) = 0;
+	virtual void bfmeSpare038BT(void) = 0;
+	virtual void bfmeSpare039BT(void) = 0;
+	virtual void bfmeSpare040BT(void) = 0;
+	virtual void bfmeSpare041BT(void) = 0;
+	virtual void bfmeSpare042BT(void) = 0;
+	virtual void bfmeSpare043BT(void) = 0;
+	virtual void bfmeSpare044BT(void) = 0;
+	virtual void bfmeSpare045BT(void) = 0;
+	virtual void bfmeSpare046BT(void) = 0;
+	virtual void bfmeSpare047BT(void) = 0;
+	virtual void bfmeSpare048BT(void) = 0;
+	virtual void bfmeSpare049BT(void) = 0;
+	virtual void bfmeSpare050BT(void) = 0;
+	virtual void bfmeSpare051BT(void) = 0;
+	virtual void bfmeSpare052BT(void) = 0;
+	virtual void bfmeSpare053BT(void) = 0;
+	virtual void bfmeSpare054BT(void) = 0;
+	virtual void bfmeSpare055BT(void) = 0;
+	virtual void bfmeSpare056BT(void) = 0;
+	virtual void bfmeSpare057BT(void) = 0;
+	virtual void bfmeSpare058BT(void) = 0;
+	virtual void bfmeSpare059BT(void) = 0;
+	virtual void bfmeSpare060BT(void) = 0;
+	virtual void bfmeSpare061BT(void) = 0;
+	virtual void bfmeSpare062BT(void) = 0;
+	virtual void bfmeSpare063BT(void) = 0;
+	virtual void bfmeSpare064BT(void) = 0;
+	virtual void bfmeSpare065BT(void) = 0;
+	virtual void bfmeSpare066BT(void) = 0;
+	virtual void bfmeSpare067BT(void) = 0;
+	virtual void bfmeSpare068BT(void) = 0;
+	virtual void bfmeSpare069BT(void) = 0;
+	virtual void bfmeSpare070BT(void) = 0;
+	virtual void bfmeSpare071BT(void) = 0;
+	virtual void bfmeSpare072BT(void) = 0;
+	virtual void bfmeSpare073BT(void) = 0;
+	virtual void bfmeSpare074BT(void) = 0;
+	virtual void bfmeSpare075BT(void) = 0;
+	virtual void bfmeSpare076BT(void) = 0;
+	virtual void bfmeSpare077BT(void) = 0;
+	virtual void bfmeSpare078BT(void) = 0;
+	virtual void bfmeSpare079BT(void) = 0;
+	virtual void bfmeSpare080BT(void) = 0;
+	virtual void bfmeSpare081BT(void) = 0;
+	virtual void bfmeSpare082BT(void) = 0;
+	virtual void bfmeSpare083BT(void) = 0;
+	virtual void bfmeSpare084BT(void) = 0;
+	virtual void bfmeSpare085BT(void) = 0;
+	virtual void bfmeSpare086BT(void) = 0;
+	virtual void bfmeSpare087BT(void) = 0;
+	virtual void bfmeSpare088BT(void) = 0;
+	virtual void bfmeSpare089BT(void) = 0;
+	virtual void bfmeSpare090BT(void) = 0;
+	virtual void bfmeSpare091BT(void) = 0;
+	virtual void bfmeSpare092BT(void) = 0;
+	virtual void bfmeSpare093BT(void) = 0;
+	virtual void bfmeSpare094BT(void) = 0;
+	virtual void bfmeSpare095BT(void) = 0;
+	virtual void bfmeSpare096BT(void) = 0;
+	virtual void bfmeSpare097BT(void) = 0;
+	virtual void bfmeSpare098BT(void) = 0;
+	virtual void bfmeSpare099BT(void) = 0;
+	virtual void bfmeSpare100BT(void) = 0;
+	virtual void bfmeSpare101BT(void) = 0;
+	virtual void bfmeSpare102BT(void) = 0;
+	virtual void bfmeSpare103BT(void) = 0;
+	virtual void bfmeSpare104BT(void) = 0;
+	virtual void bfmeSpare105BT(void) = 0;
+	virtual void bfmeSpare106BT(void) = 0;
+	virtual void bfmeSpare107BT(void) = 0;
+	virtual void bfmeSpare108BT(void) = 0;
+	virtual void bfmeSpare109BT(void) = 0;
+	virtual void bfmeSpare110BT(void) = 0;
+	virtual void bfmeSpare111BT(void) = 0;
+	virtual void bfmeSpare112BT(void) = 0;
+	virtual void bfmeSpare113BT(void) = 0;
+	virtual void bfmeSpare114BT(void) = 0;
+	virtual void bfmeSpare115BT(void) = 0;
+	virtual void bfmeSpare116BT(void) = 0;
+	virtual void bfmeSpare117BT(void) = 0;
+	virtual void bfmeSpare118BT(void) = 0;
+	virtual void bfmeSpare119BT(void) = 0;
+	virtual void bfmeSpare120BT(void) = 0;
+	virtual void bfmeSpare121BT(void) = 0;
+	virtual void bfmeSpare122BT(void) = 0;
+	virtual void bfmeSpare123BT(void) = 0;
+	virtual void bfmeSpare124BT(void) = 0;
+	virtual void bfmeSpare125BT(void) = 0;
+	virtual void bfmeSpare126BT(void) = 0;
+	virtual void bfmeSpare127BT(void) = 0;
+	virtual void bfmeSpare128BT(void) = 0;
+	virtual void bfmeSpare129BT(void) = 0;
+	virtual void bfmeSpare130BT(void) = 0;
+	virtual void bfmeSpare131BT(void) = 0;
+	virtual void bfmeSpare132BT(void) = 0;
+	virtual void bfmeSpare133BT(void) = 0;
+	virtual BfmeMadeBQ *bfmeMakeBQ(void) = 0;
+
+	void bfmeGoBQ(void);
+
+private:
+	unsigned char m_bfmeHead[0x2c];		// 0x04
+	BfmeMadeBQ *m_bfmeMade;			// 0x30
+};
+
+void BfmeThingBQ::bfmeGoBQ(void)
+{
+	if (m_bfmeMade != 0)
+		return;
+
+	BfmeMadeBQ *made = bfmeMakeBQ();
+
+	m_bfmeMade = made;
+
+	made->bfmeDoBQ();
+}
+
