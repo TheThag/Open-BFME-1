55939a2611adf2c3cd91240ca06ae435ccec6d76
argalad <github@argalad.com>
2026-08-27 18:57:09 +0200

    Open-BFME5: convert one more body
    Fifty-three members cleared, with one run of three written ahead of the block
    it belongs to.

---
+++ Code/GameEngine/Source/Common/BfmeOneHundredThirtyOne.cpp
@@ -0,0 +1,120 @@
+// One more: a reset over fifty-three members, with one run of three lifted
+// ahead of the block it belongs to.
+
+class Gen_00707170
+{
+public:
+	Gen_00707170(void);
+private:
+	int m_bfme00;						// +0x00
+	int m_bfme04;						// +0x04
+	int m_bfme08;						// +0x08
+	int m_bfme0c;						// +0x0c
+	int m_bfme10;						// +0x10
+	int m_bfme14;						// +0x14
+	int m_bfme18;						// +0x18
+	unsigned char m_bfme1c;				// +0x1c
+	unsigned char m_bfme1d;				// +0x1d
+	unsigned char m_bfme1e;				// +0x1e
+	unsigned char m_bfme1f;				// +0x1f
+	int m_bfme20;						// +0x20
+	int m_bfme24;						// +0x24
+	int m_bfme28;						// +0x28
+	int m_bfme2c;						// +0x2c
+	int m_bfme30;						// +0x30
+	int m_bfme34;						// +0x34
+	int m_bfme38;						// +0x38
+	unsigned char m_bfme3c;				// +0x3c
+	unsigned char m_bfme3d;				// +0x3d
+	unsigned char m_bfme3e;				// +0x3e
+	unsigned char m_bfme3f;				// +0x3f
+	int m_bfme40;						// +0x40
+	int m_bfme44;						// +0x44
+	int m_bfme48;						// +0x48
+	int m_bfme4c;						// +0x4c
+	int m_bfme50;						// +0x50
+	unsigned char m_bfme54;				// +0x54
+	unsigned char m_bfmeGap55[3];			// +0x55
+	int m_bfme58;						// +0x58
+	int m_bfme5c;						// +0x5c
+	int m_bfme60;						// +0x60
+	int m_bfme64;						// +0x64
+	int m_bfme68;						// +0x68
+	int m_bfme6c;						// +0x6c
+	int m_bfme70;						// +0x70
+	int m_bfme74;						// +0x74
+	int m_bfme78;						// +0x78
+	int m_bfme7c;						// +0x7c
+	int m_bfme80;						// +0x80
+	int m_bfme84;						// +0x84
+	int m_bfme88;						// +0x88
+	int m_bfme8c;						// +0x8c
+	int m_bfme90;						// +0x90
+	int m_bfme94;						// +0x94
+	int m_bfme98;						// +0x98
+	int m_bfme9c;						// +0x9c
+	int m_bfmea0;						// +0xa0
+	int m_bfmea4;						// +0xa4
+	int m_bfmea8;						// +0xa8
+	int m_bfmeac;						// +0xac
+	int m_bfmeb0;						// +0xb0
+	int m_bfmeb4;						// +0xb4
+	int m_bfmeb8;						// +0xb8
+};
+
+Gen_00707170::Gen_00707170(void)
+{
+	m_bfme00 = 0;
+	m_bfme04 = 0;
+	m_bfme08 = 0;
+	m_bfme0c = 0;
+	m_bfme10 = 0;
+	m_bfme14 = 0;
+	m_bfme18 = 0;
+	m_bfme1c = 0;
+	m_bfme1d = 0;
+	m_bfme1e = 0;
+	m_bfme1f = 0;
+	m_bfme20 = 0;
+	m_bfme24 = 0;
+	m_bfme28 = 0;
+	m_bfme2c = 0;
+	m_bfme30 = 0;
+	m_bfme34 = 0;
+	m_bfme38 = 0;
+	m_bfme3c = 0;
+	m_bfme3d = 0;
+	m_bfme3e = 0;
+	m_bfme3f = 0;
+	m_bfme40 = 0;
+	m_bfme44 = 0;
+	m_bfme48 = 0;
+	m_bfme4c = 0;
+	m_bfme50 = 0;
+	m_bfme54 = 0;
+	m_bfme58 = 0;
+	m_bfme5c = 0;
+	m_bfme60 = 0;
+	m_bfme64 = 0;
+	m_bfme68 = 0;
+	m_bfme6c = 0;
+	m_bfme70 = 0;
+	m_bfme74 = 0;
+	m_bfme78 = 0;
+	m_bfme7c = 0;
+	m_bfmea0 = 0;
+	m_bfmea4 = 0;
+	m_bfmea8 = 0;
+	m_bfme80 = 0;
+	m_bfme84 = 0;
+	m_bfme88 = 0;
+	m_bfme8c = 0;
+	m_bfme90 = 0;
+	m_bfme94 = 0;
+	m_bfme98 = 0;
+	m_bfme9c = 0;
+	m_bfmeac = 0;
+	m_bfmeb0 = 0;
+	m_bfmeb4 = 0;
+	m_bfmeb8 = 0;
+}
