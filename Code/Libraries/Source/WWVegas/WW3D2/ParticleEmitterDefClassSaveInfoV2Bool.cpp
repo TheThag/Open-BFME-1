6bbf87df44162896c937b21be90222896bc1102c
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-27 09:07:59 -0400

    Open-BFME5: convert emitter info-v2 save

---
+++ Code/Libraries/Source/WWVegas/WW3D2/ParticleEmitterDefClassSaveInfoV2Bool.cpp
@@ -0,0 +1,31 @@
+// cl: /DNDEBUG /MD /ICode/Libraries/Source/WWVegas/WWMath /ICode/Libraries/Source/WWVegas/WWLib /ICode/Libraries/Source/WWVegas/WWSaveLoad /ICode/Libraries/Source/WWVegas/WW3D2 /ICode/Libraries/Source/WWVegas/Wwutil /ICode/Libraries/Source/WWVegas/WWDownload /ICode/Libraries/Source/Compression /ICode/Libraries/Source/WWVegas/WWDebug /Ireference/shims/sweep
+// BFME's emitter save entry points return a one-byte bool. Keep this ABI view
+// local because the later vendored declaration returns WW3DErrorType.
+
+#include "winbase_shim.h"
+#define MAX_PATH 260
+#define LPCTSTR const char *
+#include "chunkio.h"
+#include "w3d_file.h"
+
+class ParticleEmitterDefClass
+{
+protected:
+	virtual bool Save_InfoV2(ChunkSaveClass &chunk_save);
+	char Pad[0x160];
+	W3dEmitterInfoStructV2 InfoV2;
+};
+
+bool ParticleEmitterDefClass::Save_InfoV2(ChunkSaveClass &chunk_save)
+{
+	bool ret_val = false;
+	if (chunk_save.Begin_Chunk(W3D_CHUNK_EMITTER_INFOV2) == TRUE)
+	{
+		if (chunk_save.Write(&InfoV2, sizeof(InfoV2)) == sizeof(InfoV2))
+		{
+			ret_val = true;
+		}
+		chunk_save.End_Chunk();
+	}
+	return ret_val;
+}
