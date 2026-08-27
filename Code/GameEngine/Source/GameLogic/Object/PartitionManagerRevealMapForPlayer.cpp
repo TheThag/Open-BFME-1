02b7ccbfa649f92116902e2de6e64df2c9a54dd4
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-27 09:24:52 -0400

    Open-BFME5: recover PartitionManager reveal delegate

---
+++ Code/GameEngine/Source/GameLogic/Object/PartitionManagerRevealMapForPlayer.cpp
@@ -0,0 +1,26 @@
+// cl: /DNDEBUG /MD /EHsc
+// BFME's public PartitionManager is a thin delegate over its implementation
+// pointer.  This TU models only the ABI slice exercised by the retail body.
+
+typedef int Int;
+
+class PartitionManagerImpl008F8D60
+{
+public:
+	void revealMapForPlayer(Int playerIndex);
+};
+
+class PartitionManager
+{
+public:
+	void revealMapForPlayer(Int playerIndex);
+
+private:
+	char m_bfmeHead[0x0c];
+	PartitionManagerImpl008F8D60 *m_impl;
+};
+
+void PartitionManager::revealMapForPlayer(Int playerIndex)
+{
+	m_impl->revealMapForPlayer(playerIndex);
+}
