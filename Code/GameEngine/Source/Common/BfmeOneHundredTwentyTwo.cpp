067f5c69bb871bfb1d9ddc7e1257047b851a608a
argalad <github@argalad.com>
2026-08-27 18:19:26 +0200

    Open-BFME5: convert one more body
    Twenty-four slot numbers handed out one global at a time, eight through
    nineteen and then twenty-seven through thirty-eight.

---
+++ Code/GameEngine/Source/Common/BfmeOneHundredTwentyTwo.cpp
@@ -0,0 +1,54 @@
+// One more: the table of slot numbers, handed out one global at a time.
+
+extern int g_bfmeSlot00VA;
+extern int g_bfmeSlot01VA;
+extern int g_bfmeSlot02VA;
+extern int g_bfmeSlot03VA;
+extern int g_bfmeSlot04VA;
+extern int g_bfmeSlot05VA;
+extern int g_bfmeSlot06VA;
+extern int g_bfmeSlot07VA;
+extern int g_bfmeSlot08VA;
+extern int g_bfmeSlot09VA;
+extern int g_bfmeSlot10VA;
+extern int g_bfmeSlot11VA;
+extern int g_bfmeSlot12VA;
+extern int g_bfmeSlot13VA;
+extern int g_bfmeSlot14VA;
+extern int g_bfmeSlot15VA;
+extern int g_bfmeSlot16VA;
+extern int g_bfmeSlot17VA;
+extern int g_bfmeSlot18VA;
+extern int g_bfmeSlot19VA;
+extern int g_bfmeSlot20VA;
+extern int g_bfmeSlot21VA;
+extern int g_bfmeSlot22VA;
+extern int g_bfmeSlot23VA;
+
+void bfmeAssignSlotsVA(void)
+{
+	g_bfmeSlot00VA = 8;
+	g_bfmeSlot01VA = 9;
+	g_bfmeSlot02VA = 10;
+	g_bfmeSlot03VA = 11;
+	g_bfmeSlot04VA = 12;
+	g_bfmeSlot05VA = 13;
+	g_bfmeSlot06VA = 14;
+	g_bfmeSlot07VA = 15;
+	g_bfmeSlot08VA = 16;
+	g_bfmeSlot09VA = 17;
+	g_bfmeSlot10VA = 18;
+	g_bfmeSlot11VA = 19;
+	g_bfmeSlot12VA = 27;
+	g_bfmeSlot13VA = 28;
+	g_bfmeSlot14VA = 29;
+	g_bfmeSlot15VA = 30;
+	g_bfmeSlot16VA = 31;
+	g_bfmeSlot17VA = 32;
+	g_bfmeSlot18VA = 33;
+	g_bfmeSlot19VA = 34;
+	g_bfmeSlot20VA = 35;
+	g_bfmeSlot21VA = 36;
+	g_bfmeSlot22VA = 37;
+	g_bfmeSlot23VA = 38;
+}
