7ea0798d1c35efe1e8f513e4bff5d1cb0a4fb9e6
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-27 09:18:18 -0400

    Open-BFME5: convert ScriptEngine camera hook

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptEngineNotifyCameraChange.cpp
@@ -0,0 +1,28 @@
+// cl: /DNDEBUG /MD /EHsc
+// BFME's ScriptEngine debug-DLL hook for the NotifyCameraChange export.
+
+typedef int (__stdcall *FARPROC)();
+typedef void *HMODULE;
+
+extern "C" __declspec(dllimport) FARPROC __stdcall GetProcAddress(
+	HMODULE module, const char *procName);
+
+extern HMODULE TheScriptDebugWindowDLL;
+
+class ScriptEngine
+{
+public:
+	void notifyCameraChange(void);
+};
+
+void ScriptEngine::notifyCameraChange(void)
+{
+	typedef void (*funcptr)(void);
+
+	if (TheScriptDebugWindowDLL)
+	{
+		FARPROC proc = GetProcAddress(TheScriptDebugWindowDLL, "NotifyCameraChange");
+		if (proc)
+			((funcptr)proc)();
+	}
+}
