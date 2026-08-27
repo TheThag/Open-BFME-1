307f6f6f2ef3d87f0fca5717fcdf20c1cbd799f2
argalad <github@argalad.com>
2026-08-27 23:36:07 +0200

    Open-BFME5: convert the count check run

---
+++ Code/GameEngine/Source/Common/BfmeOneHundredEightyOne.cpp
@@ -0,0 +1,158 @@
+// A run of checks over how many there are now and how many there were, ending
+// either quietly or by handing the last word on.
+
+class BfmeThingCJ
+{
+public:
+	virtual void bfmeSpare000CJ(void) = 0;
+	virtual void bfmeSpare001CJ(void) = 0;
+	virtual void bfmeSpare002CJ(void) = 0;
+	virtual void bfmeSpare003CJ(void) = 0;
+	virtual void bfmeSpare004CJ(void) = 0;
+	virtual void bfmeSpare005CJ(void) = 0;
+	virtual void bfmeSpare006CJ(void) = 0;
+	virtual void bfmeSpare007CJ(void) = 0;
+	virtual void bfmeSpare008CJ(void) = 0;
+	virtual void bfmeSpare009CJ(void) = 0;
+	virtual void bfmeSpare010CJ(void) = 0;
+	virtual void bfmeSpare011CJ(void) = 0;
+	virtual void bfmeSpare012CJ(void) = 0;
+	virtual void bfmeSpare013CJ(void) = 0;
+	virtual void bfmeSpare014CJ(void) = 0;
+	virtual void bfmeSpare015CJ(void) = 0;
+	virtual void bfmeSpare016CJ(void) = 0;
+	virtual void bfmeSpare017CJ(void) = 0;
+	virtual void bfmeSpare018CJ(void) = 0;
+	virtual void bfmeSpare019CJ(void) = 0;
+	virtual void bfmeSpare020CJ(void) = 0;
+	virtual void bfmeSpare021CJ(void) = 0;
+	virtual void bfmeSpare022CJ(void) = 0;
+	virtual void bfmeSpare023CJ(void) = 0;
+	virtual void bfmeSpare024CJ(void) = 0;
+	virtual void bfmeSpare025CJ(void) = 0;
+	virtual void bfmeSpare026CJ(void) = 0;
+	virtual void bfmeSpare027CJ(void) = 0;
+	virtual void bfmeSpare028CJ(void) = 0;
+	virtual void bfmeSpare029CJ(void) = 0;
+	virtual void bfmeSpare030CJ(void) = 0;
+	virtual void bfmeSpare031CJ(void) = 0;
+	virtual void bfmeSpare032CJ(void) = 0;
+	virtual void bfmeSpare033CJ(void) = 0;
+	virtual void bfmeSpare034CJ(void) = 0;
+	virtual void bfmeSpare035CJ(void) = 0;
+	virtual void bfmeSpare036CJ(void) = 0;
+	virtual void bfmeSpare037CJ(void) = 0;
+	virtual void bfmeSpare038CJ(void) = 0;
+	virtual void bfmeSpare039CJ(void) = 0;
+	virtual void bfmeSpare040CJ(void) = 0;
+	virtual void bfmeSpare041CJ(void) = 0;
+	virtual void bfmeSpare042CJ(void) = 0;
+	virtual void bfmeSpare043CJ(void) = 0;
+	virtual void bfmeSpare044CJ(void) = 0;
+	virtual void bfmeSpare045CJ(void) = 0;
+	virtual void bfmeSpare046CJ(void) = 0;
+	virtual void bfmeSpare047CJ(void) = 0;
+	virtual void bfmeSpare048CJ(void) = 0;
+	virtual void bfmeSpare049CJ(void) = 0;
+	virtual void bfmeSpare050CJ(void) = 0;
+	virtual void bfmeSpare051CJ(void) = 0;
+	virtual void bfmeSpare052CJ(void) = 0;
+	virtual void bfmeSpare053CJ(void) = 0;
+	virtual void bfmeSpare054CJ(void) = 0;
+	virtual void bfmeSpare055CJ(void) = 0;
+	virtual void bfmeSpare056CJ(void) = 0;
+	virtual void bfmeSpare057CJ(void) = 0;
+	virtual void bfmeSpare058CJ(void) = 0;
+	virtual void bfmeSpare059CJ(void) = 0;
+	virtual void bfmeSpare060CJ(void) = 0;
+	virtual void bfmeSpare061CJ(void) = 0;
+	virtual void bfmeSpare062CJ(void) = 0;
+	virtual void bfmeSpare063CJ(void) = 0;
+	virtual void bfmeSpare064CJ(void) = 0;
+	virtual void bfmeSpare065CJ(void) = 0;
+	virtual void bfmeSpare066CJ(void) = 0;
+	virtual void bfmeSpare067CJ(void) = 0;
+	virtual void bfmeSpare068CJ(void) = 0;
+	virtual void bfmeSpare069CJ(void) = 0;
+	virtual void bfmeSpare070CJ(void) = 0;
+	virtual void bfmeSpare071CJ(void) = 0;
+	virtual void bfmeSpare072CJ(void) = 0;
+	virtual void bfmeSpare073CJ(void) = 0;
+	virtual void bfmeSpare074CJ(void) = 0;
+	virtual void bfmeSpare075CJ(void) = 0;
+	virtual void bfmeSpare076CJ(void) = 0;
+	virtual void bfmeSpare077CJ(void) = 0;
+	virtual void bfmeSpare078CJ(void) = 0;
+	virtual void bfmeSpare079CJ(void) = 0;
+	virtual void bfmeSpare080CJ(void) = 0;
+	virtual void bfmeSpare081CJ(void) = 0;
+	virtual void bfmeSpare082CJ(void) = 0;
+	virtual void bfmeSpare083CJ(void) = 0;
+	virtual void bfmeSpare084CJ(void) = 0;
+	virtual void bfmeSpare085CJ(void) = 0;
+	virtual void bfmeSpare086CJ(void) = 0;
+	virtual void bfmeSpare087CJ(void) = 0;
+	virtual void bfmeSpare088CJ(void) = 0;
+	virtual void bfmeSpare089CJ(void) = 0;
+	virtual void bfmeSpare090CJ(void) = 0;
+	virtual void bfmeSpare091CJ(void) = 0;
+	virtual void bfmeSpare092CJ(void) = 0;
+	virtual void bfmeSpare093CJ(void) = 0;
+	virtual void bfmeSpare094CJ(void) = 0;
+	virtual void bfmeSpare095CJ(void) = 0;
+	virtual void bfmeSpare096CJ(void) = 0;
+	virtual void bfmeSpare097CJ(void) = 0;
+	virtual void bfmeSpare098CJ(void) = 0;
+	virtual void bfmeSpare099CJ(void) = 0;
+	virtual void bfmeSpare100CJ(void) = 0;
+	virtual void bfmeSpare101CJ(void) = 0;
+	virtual int bfmeCountCJ(void) = 0;
+	virtual void bfmeSpare103CJ(void) = 0;
+	virtual void bfmeStepCJ(void) = 0;
+	virtual void bfmeEndCJ(void) = 0;
+
+	void bfmeGoCJ(void);
+
+private:
+	unsigned char m_bfmeHead[9];		// 0x0004
+	unsigned char m_bfmeOn;			// 0x000d
+	unsigned char m_bfmeAlso;		// 0x000e
+	unsigned char m_bfmeGap[0x138d];	// 0x000f
+	int m_bfmeOther;			// 0x139c
+	int m_bfmeLast;				// 0x13a0
+};
+
+void BfmeThingCJ::bfmeGoCJ(void)
+{
+	int now = bfmeCountCJ();
+
+	if (now > 0)
+	{
+		if (m_bfmeLast == now)
+			goto bfmeCheckCJ;
+
+		if (m_bfmeOn == 0)
+			goto bfmeHandOffCJ;
+
+		if (m_bfmeAlso == 0)
+			goto bfmeCheckCJ;
+
+		bfmeStepCJ();
+
+		goto bfmeCheckCJ;
+	}
+
+	if (m_bfmeOther != 0)
+		goto bfmeHandOffCJ;
+
+bfmeCheckCJ:
+	if (m_bfmeOn == 0)
+		goto bfmeHandOffCJ;
+
+	if (m_bfmeAlso != 0)
+		return;
+
+bfmeHandOffCJ:
+	bfmeEndCJ();
+}
+
