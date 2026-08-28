e48d94b6ef4112cfbb8348df26fa5485224db35a
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-28 06:15:03 -0400

    Convert CameraShakerVolume position dispatch to C++

---
+++ Code/GameEngine/Source/GameClient/CameraShakerVolumeFXNugget_doFXPos_Thunk.cpp
@@ -0,0 +1,49 @@
+// cl: /O2 /Ob0 /DNDEBUG /MD /EHsc
+// Open-BFME5: clean C++ conversion of CameraShakerVolume's positional dispatch.
+
+struct Coord3D
+{
+	float x, y, z;
+};
+
+class Matrix3D;
+
+class TacticalView
+{
+public:
+	virtual void v00(); virtual void v04(); virtual void v08(); virtual void v0C();
+	virtual void v10(); virtual void v14(); virtual void v18(); virtual void v1C();
+	virtual void v20(); virtual void v24(); virtual void v28(); virtual void v2C();
+	virtual void v30(); virtual void v34(); virtual void v38(); virtual void v3C();
+	virtual void v40(); virtual void v44(); virtual void v48(); virtual void v4C();
+	virtual void v50(); virtual void v54(); virtual void v58(); virtual void v5C();
+	virtual void v60(); virtual void v64(); virtual void v68(); virtual void v6C();
+	virtual void v70(); virtual void v74(); virtual void v78(); virtual void v7C();
+	virtual void v80(); virtual void v84(); virtual void v88(); virtual void v8C();
+	virtual void v90(); virtual void v94(); virtual void v98(); virtual void v9C();
+	virtual void vA0();
+	virtual void addCameraShakerVolume(const Coord3D *, int, int, int);
+};
+
+extern TacticalView *TheTacticalView;
+
+class CameraShakerVolumeFXNugget
+{
+public:
+	virtual void v00();
+	virtual void doFXPos(const Coord3D *, const Matrix3D *, float, const Coord3D *) const;
+
+private:
+	unsigned char m_unmodelled[0xBC];
+	int m_fieldC0;
+	int m_fieldC4;
+	int m_fieldC8;
+};
+
+// ?doFXPos@CameraShakerVolumeFXNugget@@UBEXPBUCoord3D@@PBVMatrix3D@@M0@Z
+void CameraShakerVolumeFXNugget::doFXPos(const Coord3D *primary,
+	const Matrix3D *, float, const Coord3D *) const
+{
+	if (primary)
+		TheTacticalView->addCameraShakerVolume(primary, m_fieldC0, m_fieldC4, m_fieldC8);
+}
