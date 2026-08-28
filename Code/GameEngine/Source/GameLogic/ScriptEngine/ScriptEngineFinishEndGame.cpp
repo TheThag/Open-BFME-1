6bf46f65aeeea4ea6402f887c96089cdd5b9dcf5
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-08-27 20:34:07 -0700

    Reconstruct ScriptEngine end-game dispatch

---
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/ScriptEngineFinishEndGame.cpp
@@ -0,0 +1,50 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /O2 /GX
+
+class Rva00367810GameLogic
+{
+public:
+	bool isLivingWorld(void);
+};
+
+class CampaignManager
+{
+private:
+	char m_bfmeGap[0x1d];
+
+public:
+	bool m_victorious;
+};
+
+class MessageStream
+{
+public:
+	virtual void slot00(void); virtual void slot01(void); virtual void slot02(void);
+	virtual void slot03(void); virtual void slot04(void); virtual void slot05(void);
+	virtual void slot06(void); virtual void slot07(void); virtual void slot08(void);
+	virtual void slot09(void); virtual void slot10(void); virtual void slot11(void);
+	virtual void slot12(void);
+	virtual void appendMessage(int type);
+};
+
+class ScriptEngine
+{
+public:
+	void _bfme_finishEndGame(void);
+};
+
+#define TheGameLogic (*(Rva00367810GameLogic **)0x012F0898)
+#define TheCampaignManager (*(CampaignManager **)0x012F1024)
+#define TheMessageStream (*(MessageStream **)0x012ED5EC)
+
+void ScriptEngine::_bfme_finishEndGame(void)
+{
+	if (TheGameLogic->isLivingWorld()) {
+		if (TheCampaignManager->m_victorious) {
+			TheMessageStream->appendMessage(2009);
+		} else {
+			TheMessageStream->appendMessage(2026);
+		}
+	} else {
+		TheMessageStream->appendMessage(29);
+	}
+}
