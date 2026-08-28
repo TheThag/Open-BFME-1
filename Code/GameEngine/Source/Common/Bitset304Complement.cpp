498a7e60f27e587a85942cf8ed53c95fc0d3604e
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-28 12:26:36 -0400

    Convert STLport bitset 304 complement

---
+++ Code/GameEngine/Source/Common/Bitset304Complement.cpp
@@ -0,0 +1,9 @@
+// cl: /DNDEBUG /DWIN32 /MD /EHs-c- /D_STLP_USE_STATIC_LIB
+// stlport
+
+// Open-BFME: STLport bitset<304>::operator~, used by
+// ModelConditionFlags::clearAndSet, retail 0x001C4B30.
+
+#include <bitset>
+
+template _STL::bitset<304> _STL::bitset<304>::operator~() const;
