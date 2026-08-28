5f0690c9c3e8f97f28f00523dd53dfebbac60b1b
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-08-27 18:22:31 -0700

    Reconstruct GameEngine child process reset helper

---
+++ Code/GameEngine/Source/Common/GameEngineTerminateChildProcesses.cpp
@@ -0,0 +1,27 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /O2 /GX
+
+typedef void *HANDLE;
+typedef int BOOL;
+
+extern "C" __declspec(dllimport) BOOL __stdcall TerminateProcess(
+	HANDLE process, unsigned int exitCode);
+
+class GameEngine
+{
+public:
+	virtual void slot00(void);
+
+private:
+	void _bfme_terminateChildProcesses(void);
+
+	char m_unknown04[0x0C];
+	int m_childProcessCount;
+	HANDLE m_childProcesses[7];
+};
+
+void GameEngine::_bfme_terminateChildProcesses(void)
+{
+	for (int index = 0; index < m_childProcessCount; ++index)
+		TerminateProcess(m_childProcesses[index], 0);
+	m_childProcessCount = 0;
+}
