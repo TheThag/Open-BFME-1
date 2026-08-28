9af333ab206be46e8081153f58fd6c6f7910536b
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-28 00:33:55 -0400

    Open-BFME5: convert zone block waypoint update

---
+++ Code/GameEngine/Source/GameLogic/AI/ZoneBlockSetWaypoint.cpp
@@ -0,0 +1,42 @@
+// Maintain the compact twelve-entry waypoint array owned by a BFME ZoneBlock.
+// Erase is unordered: the final entry replaces the removed slot.
+
+typedef bool Bool;
+
+class Waypoint;
+
+class ZoneBlock
+{
+public:
+	void bfmeSetWaypoint(Bool insert, Waypoint *waypoint);
+
+private:
+	int m_numWaypoints;
+	int m_unknown04;
+	Waypoint *m_waypoints[12];
+};
+
+// ?bfmeSetWaypoint@ZoneBlock@@QAEX_NPAVWaypoint@@@Z
+void ZoneBlock::bfmeSetWaypoint(Bool insert, Waypoint *waypoint)
+{
+	if (insert)
+	{
+		if (m_numWaypoints < 12)
+		{
+			m_waypoints[m_numWaypoints] = waypoint;
+			++m_numWaypoints;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < m_numWaypoints; ++i)
+		{
+			if (m_waypoints[i] == waypoint)
+			{
+				--m_numWaypoints;
+				m_waypoints[i] = m_waypoints[m_numWaypoints];
+				break;
+			}
+		}
+	}
+}
