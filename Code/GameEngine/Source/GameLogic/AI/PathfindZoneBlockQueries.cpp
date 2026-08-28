30825ef65a6a55d4df02703438f141991b0e0af2
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-27 23:48:08 -0400

    Open-BFME5: convert zone waypoint lookup

--- Code/GameEngine/Source/GameLogic/AI/PathfindZoneBlockQueries.cpp
+++ Code/GameEngine/Source/GameLogic/AI/PathfindZoneBlockQueries.cpp
@@ -1,4 +1,5 @@
 typedef int Int;
+typedef unsigned int UnsignedInt;
 typedef bool Bool;
 
 struct ICoord2D
@@ -7,11 +8,13 @@ struct ICoord2D
 	Int y;
 };
 
+class Waypoint;
+
 struct ZoneBlock
 {
 	Int m_numWaypoints;                       // 0x000
 	Int m_unknown04;                          // 0x004
-	void *m_waypoints[12];                    // 0x008
+	Waypoint *m_waypoints[12];                // 0x008
 	unsigned char m_zoneState[0x12c - 0x38];
 	Bool m_interactsWithBridge;               // 0x12c
 	unsigned char m_tail[0x228 - 0x12d];
@@ -22,6 +25,7 @@ class PathfindZoneManager
 public:
 	Bool bfmeInteractsWithBridge(Int cellX, Int cellY) const;
 	Bool bfmeHasWaypoints(Int cellX, Int cellY) const;
+	Waypoint *bfmeGetWaypoint(Int cellX, Int cellY, UnsignedInt index) const;
 
 private:
 	unsigned char m_prefix[0x23628];
@@ -53,4 +57,21 @@ Bool PathfindZoneManager::bfmeHasWaypoints(Int cellX, Int cellY) const
 		return false;
 
 	return m_zoneBlocks[blockX][blockY].m_numWaypoints > 0;
+}
+
+Waypoint *PathfindZoneManager::bfmeGetWaypoint(
+	Int cellX, Int cellY, UnsignedInt index) const
+{
+	if (cellX < 0 || cellY < 0)
+		return 0;
+
+	Int blockX = cellX / 16;
+	Int blockY = cellY / 16;
+	if (blockX < m_zoneBlockExtent.x && blockY < m_zoneBlockExtent.y) {
+		const ZoneBlock &block = m_zoneBlocks[blockX][blockY];
+		if (index < (UnsignedInt)block.m_numWaypoints)
+			return block.m_waypoints[index];
+	}
+
+	return 0;
 }
