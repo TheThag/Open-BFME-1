f953aa9a9cb1dd1c062da2d6a137a4968aef3945
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-28 13:11:02 -0400

    Convert Object giant-bird AI query

---
+++ Code/GameEngine/Source/GameLogic/Object/Object_bfmeIsGiantBird.cpp
@@ -0,0 +1,110 @@
+// cl: /DNDEBUG /MD /EHsc
+
+typedef bool Bool;
+
+#define BFME_AI_SLOT(n) virtual void bfmeAiSlot##n();
+
+class AIUpdateInterface
+{
+public:
+	BFME_AI_SLOT(0)
+	BFME_AI_SLOT(1)
+	BFME_AI_SLOT(2)
+	BFME_AI_SLOT(3)
+	BFME_AI_SLOT(4)
+	BFME_AI_SLOT(5)
+	BFME_AI_SLOT(6)
+	BFME_AI_SLOT(7)
+	BFME_AI_SLOT(8)
+	BFME_AI_SLOT(9)
+	BFME_AI_SLOT(10)
+	BFME_AI_SLOT(11)
+	BFME_AI_SLOT(12)
+	BFME_AI_SLOT(13)
+	BFME_AI_SLOT(14)
+	BFME_AI_SLOT(15)
+	BFME_AI_SLOT(16)
+	BFME_AI_SLOT(17)
+	BFME_AI_SLOT(18)
+	BFME_AI_SLOT(19)
+	BFME_AI_SLOT(20)
+	BFME_AI_SLOT(21)
+	BFME_AI_SLOT(22)
+	BFME_AI_SLOT(23)
+	BFME_AI_SLOT(24)
+	BFME_AI_SLOT(25)
+	BFME_AI_SLOT(26)
+	BFME_AI_SLOT(27)
+	BFME_AI_SLOT(28)
+	BFME_AI_SLOT(29)
+	BFME_AI_SLOT(30)
+	BFME_AI_SLOT(31)
+	BFME_AI_SLOT(32)
+	BFME_AI_SLOT(33)
+	BFME_AI_SLOT(34)
+	BFME_AI_SLOT(35)
+	BFME_AI_SLOT(36)
+	BFME_AI_SLOT(37)
+	BFME_AI_SLOT(38)
+	BFME_AI_SLOT(39)
+	BFME_AI_SLOT(40)
+	BFME_AI_SLOT(41)
+	BFME_AI_SLOT(42)
+	BFME_AI_SLOT(43)
+	BFME_AI_SLOT(44)
+	BFME_AI_SLOT(45)
+	BFME_AI_SLOT(46)
+	BFME_AI_SLOT(47)
+	BFME_AI_SLOT(48)
+	BFME_AI_SLOT(49)
+	BFME_AI_SLOT(50)
+	BFME_AI_SLOT(51)
+	BFME_AI_SLOT(52)
+	BFME_AI_SLOT(53)
+	BFME_AI_SLOT(54)
+	BFME_AI_SLOT(55)
+	BFME_AI_SLOT(56)
+	BFME_AI_SLOT(57)
+	BFME_AI_SLOT(58)
+	BFME_AI_SLOT(59)
+	BFME_AI_SLOT(60)
+	BFME_AI_SLOT(61)
+	BFME_AI_SLOT(62)
+	BFME_AI_SLOT(63)
+	BFME_AI_SLOT(64)
+	BFME_AI_SLOT(65)
+	BFME_AI_SLOT(66)
+	BFME_AI_SLOT(67)
+	BFME_AI_SLOT(68)
+	BFME_AI_SLOT(69)
+	BFME_AI_SLOT(70)
+	BFME_AI_SLOT(71)
+	BFME_AI_SLOT(72)
+	BFME_AI_SLOT(73)
+	BFME_AI_SLOT(74)
+	BFME_AI_SLOT(75)
+	virtual Bool isGiantBird() const;
+};
+
+#undef BFME_AI_SLOT
+
+class Object
+{
+public:
+	int bfmeIsGiantBird() const;
+
+private:
+	unsigned char m_pad[0x204];
+	AIUpdateInterface *m_ai;
+};
+
+int Object::bfmeIsGiantBird() const
+{
+	AIUpdateInterface *ai = m_ai;
+	if (ai)
+	{
+		int result = ai->isGiantBird();
+		return result;
+	}
+	return 0;
+}
